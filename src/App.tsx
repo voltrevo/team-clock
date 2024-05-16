83eee8a1b0bdcf15ab9bddd3dd25ef4356778ca1
Andrew Morris <voltrevo@gmail.com>
2024-05-16 10:57:54 +1000

    set app name

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Team Clock</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -15,9 +15,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
